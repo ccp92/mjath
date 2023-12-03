a17aeb199f6019b16e5b206e64ae795409a9ea08
ccp92 <mail@chrisparsons.dev>
2023-12-03 00:26:50 +0000

    feat: update front page to be image based with some text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,12 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import css from "./page.module.css";
 
 export default function Home() {
-  const mockContentfulResponse = {
-    openForCommissions: true,
-  };
   return (
-    <main className={styles.main}>
-      <div className={styles.headerBlock}>
-        <div className={styles.headerBlock}>
-          <div>
-            {mockContentfulResponse.openForCommissions ? (
-              <span className={styles.commissionStatusOpen}>Open</span>
-            ) : (
-              <span className={styles.commissionStatusClosed}>Closed</span>
-            )}{" "}
-            for Commissions
-          </div>
-          <h1 className={styles.headerText}>Melissa Julie Anne Hicks</h1>
-          <div className={styles.headerSubText}>
-            <p>Traditional Artist</p>
-            <p>Portraiture - Concept Art - Illustration</p>
-          </div>
-        </div>
+    <main className={css.main}>
+      <div>
+        <h1>Melissa Julie Anne Hicks</h1>
+        <p>Traditional Artist</p>
+        <p>Portraiture - Concept Art - Illustration</p>
       </div>
     </main>
   );
