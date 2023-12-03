7c80e0121a243c84dd398d29770b86852fc6d320
ccp92 <mail@chrisparsons.dev>
2023-12-03 04:43:33 +0000

    feat: add dummy site links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,20 +9,16 @@ export default function Home() {
     <main className={`${css.main} ${inter.className}`}>
       <div className={css.homePage}>
         <div className={css.headerBlock}>
+          <div>
+            <p>Home - About - Gallery - Price List - Contact - Shop</p>
+          </div>
           <h1 className={css.title}>Melissa Julie Anne Hicks</h1>
           <p className={css.subtitle}>Traditional Artist</p>
           <p className={css.subsubtitle}>
             Portraiture - Concept Art - Illustration
           </p>
         </div>
-        <div className={css.imageBlock}>
-          {/* <Image
-            src={"/assets/images/main-page.webp"}
-            alt={"Drawing of upper body of nude woman in pink ink"}
-            width="800"
-            height="900"
-          /> */}
-        </div>
+        <div className={css.imageBlock}></div>
       </div>
     </main>
   );
